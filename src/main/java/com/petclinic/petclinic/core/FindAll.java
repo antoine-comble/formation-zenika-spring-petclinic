66e98002b78cf7e4161b3b4547276660ea6cc83c
Antoine <antoine.comble@zenika.com>
2026-01-20 09:16:43 +0100

    Refacto import

--- src/main/java/com/petclinic/petclinic/core/FindAll.java
+++ src/main/java/com/petclinic/petclinic/core/FindAll.java
@@ -1,8 +1,9 @@
 package com.petclinic.petclinic.core;
 
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 
-@Retention(RetentionPolicy.RUNTIME)
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
 public @interface FindAll {
 }
