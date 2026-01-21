cd0885c6f78fd249380015004fd0cdf0f09e9e13
Antoine <antoine.comble@zenika.com>
2026-01-21 14:20:17 +0100

    Exception handler : example with exception MethodArgumentTypeMismatchException

---
+++ src/main/java/com/petclinic/petclinic/tech/PetclinicExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.petclinic.petclinic.tech;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@ControllerAdvice
+public class PetclinicExceptionHandler extends Throwable {
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Object> handleException(MethodArgumentTypeMismatchException ex) {
+        String errorMessage = ex.getParameter() + " : The value for argument " + ex.getParameter().getParameterName() + " is not valid";
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+    }
+}
