b5f8e6712ea03451af92a7a39158cc46504ab530
Antoine <antoine.comble@zenika.com>
2026-01-21 10:08:48 +0100

    REST API - Expose GET endpoints : * VisitController

---
+++ src/main/java/com/petclinic/petclinic/core/VisitController.java
@@ -0,0 +1,27 @@
+package com.petclinic.petclinic.core;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/visit")
+public class VisitController {
+
+    @Autowired
+    private final VisitService visitService;
+
+    public VisitController(VisitService visitService) {
+        this.visitService = visitService;
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Visit> findById(@PathVariable Long id) {
+        return visitService.findById(id);
+    }
+
+}
